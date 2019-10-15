90802ec4428bf462bb38c05d652448ddc0144858
Erin <ErinLWeir@gmail.com>
2019-10-14 21:34:05 -0700

    Add resolution parameter and add error catch for unopened camera objects

--- rasp_pi_scripts/camera_recorder.py
+++ rasp_pi_scripts/camera_recorder.py
@@ -1,15 +1,25 @@
 import numpy as np
 import cv2
 
-def record_videofeed(camera_object, filename):
+def record_videofeed(camera_object, filename, resolution=None):
     """
     Takes in camera object and records to filename until stopped with q command
     :param camera_object: cv2.VideoCapture of device
     :param filename: filename to record to
     :return: filename.avi in $PWD
     """
-    resolution = (int(camera_object.get(3)), int(camera_object.get(4))) #pulls native resolution for the cam
-    fourcc = cv2.VideoWriter_fourcc(*'X264') # this is currently broken
+    if not camera_object.isOpened():
+        print("Camera not opened!")
+        raise ValueError
+    
+    if not resolution:
+        # pulls native res from cam and makes tuple of ints for VideoWriter object
+        resolution = (int(camera_object.get(3)), int(camera_object.get(4)))
+    
+    camera_object.set(3, resolution[0])
+    camera_object.set(4, resolution[1])
+    
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
     output = cv2.VideoWriter(f'{filename}.avi', fourcc, 30.0, resolution) # output name, encoding, FPS, resolution tuple
 
 
@@ -28,11 +38,12 @@ def record_videofeed(camera_object, filename):
             break
 
     # When everything done, release the capture and output
-    webcam.release()
+    camera_object.release()
     output.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     webcam = cv2.VideoCapture(0)
     # camera_module = cv2.VideoCapture(usePiCamera=True)
-    record_videofeed(webcam, "output_test")
+    resolution = (1280, 720)
+    record_videofeed(webcam, "space_test", resolution)
